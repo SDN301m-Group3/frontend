fe62234ac027b3f06c0dbec9db3819793138ec66
Minh <minhht.1.2.vn@gmail.com>
2024-06-21 15:40:14 +0700

    feat: add breadcrumb

--- src/app/(overview)/group/[id]/page.tsx
+++ src/app/(overview)/group/[id]/page.tsx
@@ -3,10 +3,12 @@ import CreateAlbumDialog from '@/components/overview/group/create-album-dialog';
 import GroupMembers from '@/components/overview/group/group-members';
 import GroupSettingDialog from '@/components/overview/group/group-setting/group-setting-dialog';
 import SearchAlbum from '@/components/overview/group/search-album';
+import BreadcrumbComponent from '@/components/shared/breadcrumb-component';
 import SpinLoading from '@/components/shared/spin-loading';
 import { BasicTooltip } from '@/components/shared/tool-tip';
 import { Button } from '@/components/ui/button';
 import { getGroupInfo } from '@/lib/data';
+import { BreadItem } from '@/lib/define';
 import { Settings } from 'lucide-react';
 import { Suspense } from 'react';
 
@@ -19,12 +21,28 @@ export default async function GroupPage({
 
     const group = await getGroupInfo(id);
 
+    const breadItems = [
+        {
+            title: 'Group',
+            url: '/group',
+        },
+        {
+            title: group.title,
+            url: `/group/${group._id}`,
+        },
+    ] as BreadItem[];
+
     return (
         <div className="">
             <SearchAlbum />
             <div className="my-5">
                 <GroupMembers groupId={id} />
             </div>
+
+            <div className="my-2">
+                <BreadcrumbComponent breadcrumbs={breadItems} />
+            </div>
+
             <div className="sm:flex sm:justify-between">
                 <h1 className="text-4xl font-bold">
                     <BasicTooltip title={`Group: ${group.title}`} />
--- src/app/(overview)/group/page.tsx
+++ src/app/(overview)/group/page.tsx
@@ -1,11 +1,22 @@
 import JoinedGroupList from '@/components/overview/group/joined-group-list';
 import GroupList from '@/components/overview/home/group/group-list';
+import BreadcrumbComponent from '@/components/shared/breadcrumb-component';
 import SpinLoading from '@/components/shared/spin-loading';
+import { BreadItem } from '@/lib/define';
 import { Suspense } from 'react';
 
 export default function GroupPage() {
+    const breadItems = [
+        {
+            title: 'Group',
+            url: '/group',
+        },
+    ] as BreadItem[];
     return (
         <div>
+            <div className="my-2">
+                <BreadcrumbComponent breadcrumbs={breadItems} />
+            </div>
             <span className={`text-2xl font-bold`}>My Group</span>
             <Suspense fallback={<SpinLoading />}>
                 <GroupList />
